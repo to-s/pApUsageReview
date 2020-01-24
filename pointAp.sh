638c92dc6a0feb14bfa81abdc19871e149eaae78
Tobi <tobias.weber@enisyst.de>
2020-01-24 21:12:06 +0100

    Update pointAp.sh

--- pointAp.sh
+++ pointAp.sh
@@ -3,6 +3,20 @@ COUNT=1
 PAP_AVAILABLE=0
 FUNCTION=""
 
+set_comment() {
+
+        API_HEADER="Accept: application/vnd.github.v3+json; application/vnd.github.antiope-preview+json"
+        AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+
+        curl -sSL \
+                -H "Content-Type: application/json" \
+                -H "${AUTH_HEADER}" \
+                -H "${API_HEADER}" \
+                -X POST \
+                -d "{\"body\":\"$1\"}" \
+                "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${number}/comments"
+}
+
 cd /github/workspace
 
 git diff origin/develop | grep -E '^(commit|@@|[+]\s.+\WAp)' > diff.txt
@@ -25,3 +39,4 @@ while IFS= read -r LINE; do
 
 done < diff.txt
 
+set_comment "checked"
