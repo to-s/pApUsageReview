6a2022ebbefdc1581fd5a5152ccfec27e5e398cd
Tobi <tobias.weber@enisyst.de>
2020-01-24 20:23:24 +0100

    Add files via upload

---
+++ pointAp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+COUNT=1
+PAP_AVAILABLE=0
+FUNCTION=""
+
+while IFS= read -r LINE; do
+
+        if echo $LINE | egrep '^[+]\s.+\WAp'; then
+                if [ $PAP_AVAILABLE == 1 ] ;then
+                        echo "Ap used, pAp is also available in ${FUNCTION} ${LINE}"
+#                else
+#                        echo "Ap used, because pAp is not available in ${FUNCTION} ${LINE}"
+                fi
+        elif echo $LINE | egrep '[(].+pAp.?[)]'; then
+                PAP_AVAILABLE=1
+                FUNCTION=$LINE
+        elif echo $LINE | egrep '[(].?[)]'; then
+                PAP_AVAILABLE=0
+                FUNCTION=$LINE
+        fi
+
+done < diff.txt
+
